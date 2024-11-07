ce9f392cd15927a393906ec86f35b69c6bc663f7
NahuelMendoza03 <167835726+NahuelMendoza03@users.noreply.github.com>
2024-11-07 08:24:46 -0300

    #2, #18 La vista "login" como modal en el "base".
    La vista "login" pasa a ser un modal dentro de la vista "base", la cual progresivamente se irá refinando.

---
+++ front-end/static/js/base.js
@@ -0,0 +1,17 @@
+// La etiqueta dialog ya tiene en el dom incorporado algunos métodos en js, como el de abrirlo
+// <dialogtraidoajspordom>.showModal()
+
+// La etiqueta dialog ya viene incorporado algunos métodos en js, como el de cerrarlo
+// <dialogtraidoajspordom>.close()
+
+const botonAbrirModalLogin = document.getElementById("boton-abrir-modal-login")
+const botonCerrarModalLogin = document.getElementById("boton-cerrar-modal-login")
+const modalLogin = document.getElementById("contenedor-modal-login")
+
+botonAbrirModalLogin.addEventListener("click", () => {
+    modalLogin.showModal()
+})
+
+botonCerrarModalLogin.addEventListener("click", () => {
+    modalLogin.close()
+})
