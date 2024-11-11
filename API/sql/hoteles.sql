bc3a624e1df96130e446d326e272022ae3199e52
SantiagoUBA <saguilarv@fi.uba.ar>
2024-11-11 20:30:04 -0300

    #17 modificación de datos en tabla de hoteles

--- API/sql/hoteles.sql
+++ API/sql/hoteles.sql
@@ -6,8 +6,12 @@ CREATE TABLE hoteles (
     hotel_id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     barrio VARCHAR(50) NOT NULL,
-    calle VARCHAR(50) NOT NULL,
+    direccion VARCHAR(50) NOT NULL,
     descripcion VARCHAR(200) NOT NULL,
+    servicios VARCHAR(200) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    imagen_principal VARCHAR(200) NOT NULL,
     puntuacion INT NOT NULL,
     PRIMARY KEY (hotel_id)
 );
