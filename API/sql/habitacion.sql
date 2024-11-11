21e46fecccad5a757a62b30bd569ec74b79fe812
juliM74 <jmalnero@fi.uba.ar>
2024-11-10 21:43:35 -0300

    #36 se actualiza habitacion.sql
    se código para crear la tabla habitaciones

---
+++ API/sql/habitacion.sql
@@ -0,0 +1,18 @@
+-- (para evitar errores si se vuelve a ejecutar el script (eliminar luego))
+DROP TABLE IF EXISTS habitaciones;
+
+-- Crear la tabla habitaciones
+CREATE TABLE habitaciones (
+    habitacion_id INT NOT NULL AUTO_INCREMENT,    
+    hotel_id INT NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(200) NOT NULL,
+    precio INT NOT NULL,
+    reservado BOOLEAN NOT NULL,
+    PRIMARY KEY (habitacion_id),
+    FOREIGN KEY (hotel_id) REFERENCES hoteles(hotel_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+ALTER TABLE habitaciones AUTO_INCREMENT = 30000;
