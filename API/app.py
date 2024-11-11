1d9bf2a002009e0a477e8f1ca57aa36a87ec3d1f
renzomartin1 <rsmartin@fi.uba.ar>
2024-11-10 23:46:31 -0300

    #26 Subo ejemplo de conexion y #35 creacion de la tabla reservas

--- API/app.py
+++ API/app.py
@@ -4,6 +4,19 @@ import querys
 
 app = Flask(__name__)
 
+@app.route('/api/reservas', methods=['GET'])
+def get_all_reservas():
+    try:
+        result = querys.all_reservas()
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    response = []
+    for row in result:
+        response.append({'id':row[0], 'id_reserva': row[1], 'id_habitacion':row[3], 'fecha_entrada':row[4], 'fecha_salida': row[5]})
+
+    return jsonify(response), 200
+
 
 if __name__ == "__main__":
     app.run("127.0.0.1", port="5000", debug=True)
--- API/querys.py
+++ API/querys.py
@@ -3,4 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 
+QUERY_TODAS_LAS_RESERVAS = "SELECT id, id_reserva, id_habitacion, fecha_entrada, fecha_salida FROM reservas"
 
+# string de conexión a la base de datos: mysql://usuario:password@host:puerto/nombre_schema
+#engine = create_engine("mysql://root:root@localhost:3308/IDS_API")
+engine = create_engine("mysql+mysqlconnector://renzo:123@localhost:3306/IDS_API")
+
+def run_query(query, parameters=None):
+    with engine.connect() as conn:
+        result = conn.execute(text(query), parameters)
+        conn.commit()
+    return result
+
+def all_reservas():
+    return run_query(QUERY_TODAS_LAS_RESERVAS).fetchall()
