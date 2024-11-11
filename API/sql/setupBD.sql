65701f18023d349896985dd6a1dbddf71dda8eaa
juliM74 <jmalnero@fi.uba.ar>
2024-11-10 21:35:06 -0300

    #45 se crea setupBD.sql
    se crea para conectar o llamar los distintos archivos (.sql) y montar la base de datos

---
+++ API/sql/setupBD.sql
@@ -0,0 +1,9 @@
+-- Crear base de datos
+CREATE DATABASE IF NOT EXISTS hospedajes;
+USE hospedajes;
+
+-- Incluir archivos de tablas
+SOURCE usuarios.sql;
+SOURCE hoteles.sql;
+SOURCE habitaciones.sql;
+SOURCE reservas.sql;
