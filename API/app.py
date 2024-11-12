1a6e2184d8ccc35979a91307e5b11e77e2b093de
limtnez <limartinez@fi.uba.ar>
2024-11-12 16:34:07 -0300

    #28 Se crearon dos endpoints, uno para obtener la información de todo usuario y otra para obtenerla de uno solo dado su ID.

--- API/app.py
+++ API/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, request
-
 import querys
 
 app = Flask(__name__)
+PORT = 5000
 
-@app.route('/api/reservas', methods=['GET'])
+@app.route('/api/reservas', methods = ['GET'])
 def get_all_reservas():
     try:
         result = querys.all_reservas()
@@ -13,10 +13,53 @@ def get_all_reservas():
 
     response = []
     for row in result:
-        response.append({'id':row[0], 'id_reserva': row[1], 'id_habitacion':row[3], 'fecha_entrada':row[4], 'fecha_salida': row[5]})
+        response.append({
+            'id': row[0], 
+            'id_reserva': row[1],
+            'id_habitacion': row[2], 
+            'fecha_entrada': row[3], 
+            'fecha_salida': row[4]
+        })
 
     return jsonify(response), 200
 
+
+@app.route('/api/usuarios', methods = ['GET'])
+def obtener_todos_los_usuarios():
+    try:
+        result = querys.obtener_todos_los_usuarios()
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    response = []
+    for row in result:
+        response.append({
+            'nombre': row[0],
+            'apellido': row[1],
+            'email': row[2],
+            'numero': row[3]
+        })
+
+    return jsonify(response), 200
+
+
+@app.route('/api/usuarios/<int:usuario_id>', methods = ['GET'])
+def obtener_usuario_por_id(usuario_id):
+    try:
+        result = querys.obtener_usuario_by_id(usuario_id)
+    except Exception as e:
+        return jsonify({ 'error': str(e) }), 404
+    
+    if result is None:
+        return jsonify({ 'error': 'No se ha encontrado un usuario con el ID dado' })
+    
+    response = {
+        'nombre': result[0],
+        'apellido': result[1],
+        'email': result[2],
+        'numero': result[3]
+    }
+    return jsonify(response), 200
 
 @app.route('/api/hoteles', methods=['GET'])
 def filtrar_hoteles():
@@ -50,4 +93,4 @@ def filtrar_hoteles():
 
 
 if __name__ == "__main__":
-    app.run("127.0.0.1", port="5000", debug=True)
+    app.run("127.0.0.1", port = PORT, debug = True)
--- API/querys.py
+++ API/querys.py
@@ -3,8 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 
+# Usuarios
+QUERY_OBTENER_TODOS_LOS_USUARIOS = "SELECT nombre, apellido, email, numero FROM usuarios"
+QUERY_OBTENER_USUARIO_POR_ID = "SELECT nombre, apellido, email, numero FROM usuarios WHERE usuario_id = :usuario_id"
+
+# Reservas
 QUERY_TODAS_LAS_RESERVAS = "SELECT id, id_reserva, id_habitacion, fecha_entrada, fecha_salida FROM reservas"
 
+# Hoteles
 QUERY_FILTRAR_HOTELES = """
 SELECT h.hotel_id, h.imagen_principal, h.barrio, h.nombre, h.descripcion, h.direccion FROM hoteles h
 INNER JOIN habitaciones hab ON h.hotel_id = hab.hotel_id
@@ -15,14 +21,20 @@ GROUP BY h.hotel_id;
 
 
 # string de conexión a la base de datos: mysql://usuario:password@host:puerto/nombre_schema
-#engine = create_engine("mysql://root:root@localhost:3308/IDS_API")
+# engine = create_engine("mysql://root:root@localhost:3308/IDS_API")
 engine = create_engine("mysql+mysqlconnector://renzo:123@localhost:3306/hospedajes")
 
-def run_query(query, parameters=None):
+def run_query(query, parameters = None):
     with engine.connect() as conn:
         result = conn.execute(text(query), parameters)
         conn.commit()
     return result
+
+def obtener_todos_los_usuarios():
+    return run_query(QUERY_OBTENER_TODOS_LOS_USUARIOS).fetchall()
+
+def obtener_usuario_by_id(usuario_id):
+    return run_query(QUERY_OBTENER_USUARIO_POR_ID, { 'usuario_id': usuario_id }).fetchone()
 
 def all_reservas():
     return run_query(QUERY_TODAS_LAS_RESERVAS).fetchall()
