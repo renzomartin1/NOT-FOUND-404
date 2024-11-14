28baa5cce14edbb12e9a47a4d88c55b81afd36d4
Tomas Kopal <tkopal@fi.uba.ar>
2024-11-14 04:14:27 -0300

    #35 agregado de FOREIGN KEYS a la tabla reservaciones

--- API/sql/reservaciones.sql
+++ API/sql/reservaciones.sql
@@ -1,17 +1,13 @@
 USE hospedajes;
 DROP TABLE IF EXISTS reservaciones;
-CREATE TABLE IF NOT EXISTS reservaciones(
-    id INT NOT NULL AUTO_INCREMENT,
-    reserva_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS reservaciones (
+    reserva_id INT NOT NULL AUTO_INCREMENT,
     usuario_id INT NOT NULL,
     hotel_id INT NOT NULL,
-    habitacion_id VARCHAR(100),
+    habitacion_id INT NOT NULL,
     fecha_entrada DATE NOT NULL,
     fecha_salida DATE NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (reserva_id),
+    FOREIGN KEY (hotel_id) REFERENCES hoteles(hotel_id),
+    FOREIGN KEY (habitacion_id) REFERENCES habitaciones(habitacion_id)
 );
-
-INSERT INTO reservaciones (reserva_id, usuario_id, hotel_id, habitacion_id, fecha_entrada, fecha_salida)
-VALUES (1234,2001,02,'9-B','2024-2-23 00:00:00','2024-3-23 00:00:00');
-INSERT INTO reservaciones (reserva_id, usuario_id, hotel_id, habitacion_id, fecha_entrada, fecha_salida)
-VALUES (4567,2002,07,'17-A','2024-3-23 00:00:00', '2024-4-23 00:00:00');
