e994f0e2c9a3b870efd5a395a26c80fe13290cbd
renzomartin1 <rsmartin@fi.uba.ar>
2024-11-15 20:34:30 -0300

    #35 subo tabla sql reservaciones a rama bakcend para luego mergear a develpment

--- API/sql/reservaciones.sql
+++ API/sql/reservaciones.sql
@@ -8,6 +8,11 @@ CREATE TABLE IF NOT EXISTS reservaciones (
     fecha_entrada DATE NOT NULL,
     fecha_salida DATE NOT NULL,
     PRIMARY KEY (reserva_id),
-    FOREIGN KEY (hotel_id) REFERENCES hoteles(hotel_id),
+    FOREIGN KEY (hotel_id) REFERENCES hoteles(hotel_id)
+            ON DELETE CASCADE
+			ON UPDATE CASCADE,
     FOREIGN KEY (habitacion_id) REFERENCES habitaciones(habitacion_id)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE
 );
+ALTER TABLE reservaciones AUTO_INCREMENT = 40000;
