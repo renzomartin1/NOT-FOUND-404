ce0e82e5fa67c561d5c027f8acef016ad1408f03
NahuelMendoza03 <167835726+NahuelMendoza03@users.noreply.github.com>
2024-11-22 07:54:09 -0300

    #2, #18, #19 Scripts para forzar la apertura.
    Implementación de los scripts para forzar la apertura de los modales login y register según corresponda.

---
+++ front-end/static/js/forzar_modal_login.js
@@ -0,0 +1,6 @@
+modalLogin.showModal()
+/*
+Este script fuerza la apertura del modal login y solo se ejecutará cuando se renderice home.html luego de que:
+
+- Desde el modal register se registra un usuario correctamente (mensaje de éxito).
+*/
